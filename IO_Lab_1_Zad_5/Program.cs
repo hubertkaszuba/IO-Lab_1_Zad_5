099787183c03c7141716fc8f992f0dc9f987ec66
hubertkaszuba <hubert.kaszuba@gmail.com>
2017-11-15 01:25:59 +0100

    Commit

--- IO_Lab_1_Zad_5/Program.cs
+++ IO_Lab_1_Zad_5/Program.cs
@@ -13,16 +13,33 @@ namespace IO_Lab_1_Zad_5
     {
         public static int SIZE_Tab = 256;
         public static int SIZE_Frag = 32;
-
+        public static int Sum = 0;
+        static int[] tab = new int[SIZE_Tab];
+        private static AutoResetEvent event_1 = new AutoResetEvent(true);
         static void Main(string[] args)
         {
-            int[] tab = new int[SIZE_Tab];
+            
             Random rnd = new Random();
             for (int i = 0; i < SIZE_Tab; i++)
                 tab[i] = rnd.Next(0, 100);
             for (int i = 0; i < SIZE_Tab; i++)
                 Console.WriteLine(tab[i] + " ");
-               
+            int count = SIZE_Tab / SIZE_Frag;
+            for(int i = 0; i < count; i++)
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadSum), new object[] { i * SIZE_Frag, (i + 1) * SIZE_Frag });
+
+            //WaitHandle.WaitAll();
+            Console.WriteLine(Sum);
+
+        }
+        static void ThreadSum(Object stateInfo)
+        {
+            var start_pom = ((object[])stateInfo)[0];
+            var end_pom = ((object[])stateInfo)[1];
+            int start = (int)start_pom;
+            int end = (int)end_pom;
+            int pom = sum(tab, start, end);
+            Sum += pom;
         }
 
         static int sum(int[] tab, int start, int end)
